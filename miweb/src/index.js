3c9a504cb9d9e7864811d9af784628b77f462d6f
Axel Rodriguez <rodriguez.frias.axelisaias@gmail.com>
2024-08-08 21:35:21 -0600

    mi tercer commit

---
+++ miweb/src/index.js
@@ -0,0 +1,15 @@
+const fn = ()=> {
+    const Cam ="Hi camila!"
+    let b = Cam + " testraño"
+    console.log(b)
+    console.log("Hola wordllll")
+    console.log('damnnnnn');
+    
+}
+
+fn()
+const clickeado = () =>
+{
+    let a = "troste"
+    console.log(a)
+}
